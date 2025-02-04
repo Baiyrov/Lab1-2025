645b4fa5fd9bfdf2851d621712a358b2f694fd7d
Baiyrov <y_baiyrov@kbtu.kz>
2025-02-04 23:00:54 +0500

    update

---
+++ lab3/functions1/task3.py
@@ -0,0 +1,10 @@
+def check(a,b):
+    return b//2-a
+
+def check(a,b):
+    return 2*a-b//2
+
+a=int(input())
+b=int(input())
+print("The amount of rabbits: ", check(a,b))
+print("The amount of chickens: ", check(a,b))
